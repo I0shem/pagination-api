0bd4111c2b7e4e5381dd99863d93c220b1718853
I0shem <115866131+I0shem@users.noreply.github.com>
2023-02-20 23:19:23 +0200

    pagination rework

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,8 +3,14 @@ import s from "./Pages.module.css";
 
 const Pagination = (CountElem, totalCount) => {
   const pageNumber = [];
-  for (let i = 1; i <= totalCount / CountElem; i++) {}
-  return <div className={s.Pagination}>Pagination</div>;
+  for (let i = 1; i <= totalCount / CountElem; i++) {
+    pageNumber.push(i);
+  }
+  return (
+    <div className={s.paginationBox}>
+      <ul className={s.paginationList}>{}</ul>
+    </div>
+  );
 };
 
 export default Pagination;
