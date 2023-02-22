a0e3c5783be405bc4867a4310d3f4bbb33407023
I0shem <115866131+I0shem@users.noreply.github.com>
2023-02-22 17:14:30 +0200

    pagination complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import s from "./app.module.css";
 import axios from "axios";
 import Pages from "./components/Pages";
+import Pagination from "./components/Pagination";
 
 function App() {
   const [images, setImages] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [countElem, setCountElem] = useState(10);
+  const [countElem] = useState(100);
+  const [btnText, setBtnText] = useState("Click me to scroll down");
 
   useEffect(() => {
     axios
@@ -18,17 +20,58 @@ function App() {
       .catch((error) => {
         console.log(error);
       });
-  }, [images]);
+  }, []);
 
   const lastImageIndex = currentPage * countElem;
   const firstImageIndex = lastImageIndex - countElem;
 
   const currentImages = images.slice(firstImageIndex, lastImageIndex);
+  const paginationPoint = (pageNumber) => {
+    handleButtonClick();
+    setCurrentPage(pageNumber);
+  };
 
+  const paginationPointBack = (pageNumber) => {
+    if (pageNumber >= 2) {
+      handleButtonClick();
+      setCurrentPage(pageNumber - 1);
+    } else {
+    }
+  };
+
+  const paginationPointForward = (pageNumber) => {
+    if (pageNumber < Math.ceil(images.length / countElem)) {
+      handleButtonClick();
+      setCurrentPage(pageNumber + 1);
+    } else {
+    }
+  };
+  const [isScrollingTop, setIsScrollingTop] = useState(false);
+  const handleButtonClick = () => {
+    if (isScrollingTop) {
+      setBtnText("Click me to scroll down");
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    } else {
+      setBtnText("Click me to scroll up");
+      window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+    }
+    setIsScrollingTop(!isScrollingTop);
+  };
   return (
     <div className={s.App}>
+      <button className={s.scrollDownBtn} onClick={handleButtonClick}>
+        {btnText}
+      </button>
       <div className={s.Images}>
-        <Pages images={images} />
+        <Pages images={currentImages} />
+        <Pagination
+          currentPage={currentPage}
+          countElem={countElem}
+          totalCount={images.length}
+          point={paginationPoint}
+          pointBack={paginationPointBack}
+          pointForward={paginationPointForward}
+        />
       </div>
     </div>
   );
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,14 +1,49 @@
 import React from "react";
 import s from "./Pages.module.css";
 
-const Pagination = (CountElem, totalCount) => {
-  const pageNumber = [];
-  for (let i = 1; i <= totalCount / CountElem; i++) {
-    pageNumber.push(i);
+const Pagination = ({
+  currentPage,
+  countElem,
+  totalCount,
+  point,
+  pointBack,
+  pointForward,
+}) => {
+  const pages = [];
+  for (let i = 1; i <= Math.ceil(totalCount / countElem); i++) {
+    pages.push(i);
   }
   return (
     <div className={s.paginationBox}>
-      <ul className={s.paginationList}>{}</ul>
+      <ul className={s.paginationList}>
+        <a className={s.Arrow} onClick={() => pointBack(currentPage)}>
+          {" "}
+          ←{" "}
+        </a>
+        {pages.map((number) => {
+          return (
+            <li className={s.paginationListItem} key={number}>
+              {number === currentPage ? (
+                <a
+                  className={s.highlight}
+                  href="#"
+                  onClick={() => point(number)}
+                >
+                  {number}
+                </a>
+              ) : (
+                <a href="#" onClick={() => point(number)}>
+                  {number}
+                </a>
+              )}
+            </li>
+          );
+        })}
+        <a className={s.Arrow} onClick={() => pointForward(currentPage)}>
+          {" "}
+          →{" "}
+        </a>
+      </ul>
     </div>
   );
 };
